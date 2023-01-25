6d2eb24849ca1d843247dd76714d981b9d1eefed
kelseyjking <kking11@chicagobooth.edu>
2023-01-25 20:12:35 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do  
   # PART 1: EACH IN ERB
   # ===================
-
-  get("/lottery/lucky", { :controller => "numbers", :action => "winners" })
   get("/", { :controller => "numbers", :action => "winners" })
-
+  get("/lottery/lucky", { :controller => "numbers", :action => "winners" })
+  get("/lottery/unlucky", { :controller => "numbers", :action => "losers" })
   # Let users visit URLs:
 
   # - /lottery/unlucky
@@ -15,8 +14,8 @@ Rails.application.routes.draw do
   # Uncomment each route below ONE AT A TIME and debug.
   # Do NOT uncomment more than one at a time, or you'll be dealing with multiple syntax errors at once.
 
-  # get("/zodiacs/aries", { :controller =>  fire, action =>  "ram" })
-  # get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
+  get("/zodiacs/aries", { :controller =>  "fire", :action =>  "ram" })
+  get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
   # get("/zodiacs/sagittarius" { :controller => "fire", :action => "archer" })
   
   # get("/zodiacs/taurus", { :controller, "earth", :action, "bull" })
